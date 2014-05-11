945db36c628819a39647870d715d6f86e8338eff
Duvig <piet.duvig@gmail.com>
2014-05-11 15:42:34 +0200

    test

---
+++ Assets/Scripts/BulletControl.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.rigidbody2D.AddForce ( transform.TransformDirection(Vector3.right) * 100);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+	}
+}
