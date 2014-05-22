e17fbb5ff6850e01dea69571d0ade905464af063
Stlcks <johs.duvig@gmail.com>
2014-05-22 09:09:33 +0200

    NGUI added

---
+++ Assets/GUIManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIManager : MonoBehaviour {
+	
+	private static GUIManager instance;
+	
+	public static GUIManager Instance
+	{
+	get{
+		if(instance == null)
+			instance = new GUIManager();
+			return instance;
+		}
+	}
+
+	public string PlayerLabelText;
+	public UILabel PlayerNameLabel;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
