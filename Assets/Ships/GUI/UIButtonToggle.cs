f252c40ee7829dae63e0f191f68730bb464ae73a
stlck <johs.duvig@gmail.com>
2014-05-23 23:38:48 +0200

    CommandScreen Added

---
+++ Assets/Ships/GUI/UIButtonToggle.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIButtonToggle : MonoBehaviour {
+
+	public GameObject target;
+	public bool state = true;
+	
+	void OnClick () { 
+		state = !NGUITools.GetActive(target);
+		if (target != null) 
+			NGUITools.SetActive(target, state); 
+	}
+}
---
+++ Assets/Ships/GUI/UISlideTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UISlideTrigger : MonoBehaviour {
+
+	public bool State = false;
+	Animator _anim;
+
+	void Start()
+	{
+		_anim = GetComponent< Animator>();
+	}
+
+	public void Slide()
+	{
+		_anim.SetTrigger ("Slide");
+	}
+}
